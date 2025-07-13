5a792cce3e220405e4b5c86848a272a0451daffc
Tanmay Gosavi <try.tanmayy@gmail.com>
2025-07-13 12:04:14 +0530

    Cleaned up the states

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -3,20 +3,29 @@ import React, { useEffect, useState } from 'react'
 import Pokemons from './Pokemons'
 
 const PokeCard = () => {
-    const [pokeApi,setPokeApi] = useState("https://pokeapi.co/api/v2/pokemon")
-    const [pokemons , setPokemons] = useState([])
-    const [nextUrl , setNextUrl] = useState('')
-    const [prevUrl , setPrevUrl] = useState('')
-    const [isLoading , setIsLoading] = useState(true)
+    // const [pokeApi,setPokeApi] = useState("https://pokeapi.co/api/v2/pokemon")
+    // const [pokemons , setPokemons] = useState([])
+    // const [nextUrl , setNextUrl] = useState('')
+    // const [prevUrl , setPrevUrl] = useState('')
+    // const [isLoading , setIsLoading] = useState(true)
 
+    const [PokemonList,setPokemonList] = useState({
+        pokeApi : "https://pokeapi.co/api/v2/pokemon" ,
+        pokemons : [] ,
+        nextUrl : '' ,
+        prevUrl : '' ,
+        isLoading : true
+	})
     useEffect(()=>{
         fetchCurrData()
-    },[pokeApi])
+    },[PokemonList.pokeApi])
 
     const fetchCurrData = async ()=>{
-        const poke = await axios.get(pokeApi) //this downloads the list of 20 pokemons
-        setNextUrl(poke.data.next)
-        setPrevUrl(poke.data.previous)
+        const poke = await axios.get(PokemonList.pokeApi) //this downloads the list of 20 pokemons
+        // setNextUrl(poke.data.next)
+        // setPrevUrl(poke.data.previous)
+        setPokemonList((state)=>({...state,nextUrl : poke.data.next , prevUrl : poke.data.previous}))
+
         const pokeDataResults = poke.data.results  //this gives array of name and url of  pokemons
         const indiviualPokeData = pokeDataResults.map(ele=>axios.get(ele.url))  //From their indiviual urls , we get array of promises
         const pokemonData = await axios.all(indiviualPokeData)  //Wait till all promises get fulfilled
@@ -27,24 +36,25 @@ const PokeCard = () => {
                 image : ele.data.sprites.other.dream_world.front_default ? ele.data.sprites.other.dream_world.front_default :  ele.data.sprites.back_shiny
             }
         })
-        setPokemons(res)    
-        setIsLoading(false)
+        // setPokemons(res)    
+        // setIsLoading(false)
+        setPokemonList(state =>({...state, pokemons : res , isLoading : false}))    
     }
   return (
     <>  
     {
-        isLoading ? <h1 className='text-2xl text-white p-20 text-center font-bold'>Loading...</h1> : ""
+        PokemonList.isLoading ? <h1 className='text-2xl text-white p-20 text-center font-bold'>Loading...</h1> : ""
     }
         <div className='flex flex-wrap justify-center items-center gap-5 pt-20 pb-15 px-10'>
             {
-                pokemons.map((ele,idx)=>{
+                PokemonList.pokemons.map((ele,idx)=>{
                     return <Pokemons key={idx} name={ele.name} id={ele.id} image={ele.image} />
                 })
             }
         </div>
         <div className='flex space-x-5'>
-            <button disabled={prevUrl==null} onClick={()=>setPokeApi(prevUrl)} className='px-8 py-2 text-sm text-white font-bold text-center bg-[#413f3f4f] rounded-md cursor-pointer'>Previous</button>
-            <button disabled={nextUrl==undefined} onClick={()=>setPokeApi(nextUrl)} className='px-8 py-2 text-sm text-white font-bold text-center bg-[#413f3f4f] rounded-md cursor-pointer'>Next</button>
+            <button disabled={PokemonList.prevUrl==null} onClick={()=>setPokemonList({...PokemonList, pokeApi : PokemonList.prevUrl})} className='px-8 py-2 text-sm text-white font-bold text-center bg-[#413f3f4f] rounded-md cursor-pointer'>Previous</button>
+            <button disabled={PokemonList.nextUrl==null} onClick={()=>setPokemonList({...PokemonList, pokeApi : PokemonList.nextUrl})} className='px-8 py-2 text-sm text-white font-bold text-center bg-[#413f3f4f] rounded-md cursor-pointer'>Next</button>
         </div>
     </>
   )
